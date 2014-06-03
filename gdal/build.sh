28b0b5be1bec10910d2697da544b62c8c27b2f58
Daniel Clewley <daniel.clewley@gmail.com>
2014-06-03 08:10:35 -0700

    Copy GDAL data.

--- gdal/build.sh
+++ gdal/build.sh
@@ -3,11 +3,14 @@
 ./configure --with-python --prefix=$PREFIX \
 --with-geos=$PREFIX/bin/geos-config \
 --with-static-proj4=$PREFIX \
---with-hdf5=$PREFIX
+--with-hdf5=$PREFIX \
+--with-xerces=$PREFIX \
+--disable-rpath
 make
 make install
 
 # Copy data files 
+mkdir -p $PREFIX/share/gdal/
 cp data/*csv $PREFIX/share/gdal/
 cp data/*wkt $PREFIX/share/gdal/
 
