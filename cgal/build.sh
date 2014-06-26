516886371b527711ba42c2708539cbb1825ed318
Pete Bunting <petebunting@mac.com>
2014-06-26 12:27:40 +0100

    Updated CGAL so OpenGL is found on 10.9 Mac os x and not compiled with qt - don't need it for our build.

--- cgal/build.sh
+++ cgal/build.sh
@@ -13,6 +13,8 @@ if [ `uname` == Darwin ]; then
     -D GMPXX_LIBRARIES=$PREFIX/include/libgmpxx.dylib \
     -D ZLIB_LIBRARY=$PREFIX/lib/libz.dylib \
     -D ZLIB_INCLUDE_DIR=$PREFIX/include \
+    -D WITH_OpenGL=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/System/Library/Frameworks/OpenGL.framework \
+	-D OPENGL_INCLUDE_DIR=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/System/Library/Frameworks/OpenGL.framework/Headers \
     .
 else
     cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
