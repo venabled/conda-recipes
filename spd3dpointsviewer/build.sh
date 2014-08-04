1f1be8d680ffb54d157dc156045b254b6362ac43
Daniel Clewley <daniel.clewley@gmail.com>
2014-08-04 21:10:34 +0100

    Added recipe for SPDPointsViewer - on Linux need to build against system Qt, runs OK against conda qt though.

---
+++ spd3dpointsviewer/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ `uname` == Darwin ]; then
+    export DYLD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
+else
+    export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
+fi
+
+cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
+-D CMAKE_VERBOSE_MAKEFILE=ON \
+-D SPDLIB_IO_INCLUDE_DIR=$PREFIX/include \
+-D SPDLIB_IO_LIB_PATH=$PREFIX/lib \
+-D HDF5_INCLUDE_DIR=$PREFIX/include \
+-D HDF5_LIB_PATH=$PREFIX/lib \
+-D GDAL_INCLUDE_DIR=$PREFIX/include \
+-D GDAL_LIB_DIR=$PREFIX/lib \
+.
+
+make
+make install
+
