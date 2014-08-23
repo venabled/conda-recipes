5e91498d37013c66d593ad2facbeb55ad4990572
Daniel Clewley <daniel.clewley@gmail.com>
2014-08-23 09:30:26 -0700

    Updated R for latest version. Build without X.

--- r/build.sh
+++ r/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-./configure --enable-R-shlib --with-readline=no --with-tcl-config=$PREFIX/lib/tclConfig.sh --with-tk-config=$PREFIX/lib/tkConfig.sh --prefix=$PREFIX
+./configure --enable-R-shlib \
+--with-readline=no \
+--with-tcl-config=$PREFIX/lib/tclConfig.sh \
+--with-tk-config=$PREFIX/lib/tkConfig.sh \
+--prefix=$PREFIX \
+--with-x=no
+
 make
 make install
