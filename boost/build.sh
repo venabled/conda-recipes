df9b844e1c0ad07501a52df2d0ec0ba10457e130
Daniel Clewley <daniel.clewley@gmail.com>
2014-08-25 04:32:43 -0700

    Modified boost build instructions for Python 3.4

--- boost/build.sh
+++ boost/build.sh
@@ -3,7 +3,8 @@
 # Hints:
 # http://boost.2283326.n4.nabble.com/how-to-build-boost-with-bzip2-in-non-standard-location-td2661155.html
 # http://www.gentoo.org/proj/en/base/amd64/howtos/?part=1&chap=3
-
+#
+# Python 3 build - http://thb.lt/blog/2014/boost-python-3-osx.html
 # Build dependencies:
 # - bzip2-devel
 
@@ -16,9 +17,11 @@ else
     export LD_LIBRARY_PATH=$PREFIX/lib
 fi
 
-#ln -s $PREFIX/lib/python3.4m $PREFIX/lib/python3.4
-
-./bootstrap.sh --prefix="${PREFIX}" --with-python=$PREFIX/bin/python --with-python-root=$PREFIX  --with-libraries=all
-./b2
+./bootstrap.sh --prefix="${PREFIX}" \
+ --with-python-version=3.4 \
+ --with-python=$PYTHON \
+ --with-python-root=$PREFIX  \
+ --with-libraries=all
+./b2 include="$PREFIX/include/python3.4m"
 ./bjam install
 cp bjam $PREFIX/bin
