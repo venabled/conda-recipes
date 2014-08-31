ec018ea7e940d5b2302219b20292a8e0b12d8b3b
Daniel Clewley <daniel.clewley@gmail.com>
2014-08-31 14:05:22 +0100

    Updated SPDPoints viewer to specify SPDLib version.

--- spd3dpointsviewer/build.sh
+++ spd3dpointsviewer/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Need to build against system QT - make sure conda qt is not installed.
+
 if [ `uname` == Darwin ]; then
     export DYLD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
 else
@@ -14,6 +16,7 @@ cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
 -D HDF5_LIB_PATH=$PREFIX/lib \
 -D GDAL_INCLUDE_DIR=$PREFIX/include \
 -D GDAL_LIB_DIR=$PREFIX/lib \
+-D CMAKE_BUILD_TYPE=DEBUG \
 .
 
 make
