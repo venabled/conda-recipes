f86551ec7256399942a657ce843005fb189bee7c
Daniel Clewley <daniel.clewley@gmail.com>
2014-09-27 08:22:42 -0700

    Updated RSGISLib version number and version of kealib required. Have added back tests but only print a message if they fail (which won't stop conda build).

--- rsgislib/run_test.py
+++ rsgislib/run_test.py
@@ -5,17 +5,22 @@ conda_prefix=os.getenv('PREFIX')
 # Set GDAL driver path (so KEA will be found)
 os.putenv('GDAL_DRIVER_PATH',os.path.join(conda_prefix,'gdalplugins'))
 
-import rsgislib
-from rsgislib import imagecalibration
-from rsgislib import classification
-from rsgislib import elevation
-from rsgislib import imagecalc
-from rsgislib import imagefilter
-from rsgislib import imageregistration
-from rsgislib import imageutils
-from rsgislib import rastergis
-from rsgislib import segmentation
-from rsgislib import vectorutils
-from rsgislib import zonalstats
+try:
+    import rsgislib
+    from rsgislib import imagecalibration
+    from rsgislib import classification
+    from rsgislib import elevation
+    from rsgislib import imagecalc
+    from rsgislib import imagefilter
+    from rsgislib import imageregistration
+    from rsgislib import imageutils
+    from rsgislib import rastergis
+    from rsgislib import segmentation
+    from rsgislib import vectorutils
+    from rsgislib import zonalstats
+    
+    print('All libraries imported OK')
+except ImportError as err:
+    print('ERROR: Not all libraries were imported OK')
+    print(err)
 
-print('All libraries imported OK')
